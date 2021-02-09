d1dd06fbc77622ae19f3e06faf30a39127768967
Rupangkan <every1isnotrupangkan@gmail.com>
2021-02-09 09:01:14 +0530

    Creating a batch file for git commands

--- src/main/java/ProjectInitializerAutomation/ProjectInitializerAutomation/Local.java
+++ src/main/java/ProjectInitializerAutomation/ProjectInitializerAutomation/Local.java
@@ -1,6 +1,12 @@
 package ProjectInitializerAutomation.ProjectInitializerAutomation;
 
+import java.io.BufferedWriter;
+import java.io.Console;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 public class Local {
 	private String path = "D:\\Projects\\";
@@ -12,15 +18,17 @@ public class Local {
 			boolean bool = file.mkdir();
 			path = path+rep;
 			if(bool){
-				System.out.println("Directory created successfully");
-				createFolder(path);
+				//make directory name dynamic
+				System.out.println("Projects Directory created successfully");
+				
 			}else{
-				System.out.println("File already there");
-				createFolder(path);
+				System.out.println("Projects folder already there");
+				
 			}
+			createFolder(path);
 		}
 		catch(Exception e) {
-			System.out.println("Could not create file");
+			System.out.println("Could not create folder");
 		}
 	}
 	private void createFolder(String path) {
@@ -29,26 +37,75 @@ public class Local {
 			boolean bool = file.mkdir();
 			if(bool){
 				System.out.println("Directory created successfully");
-				command();
+				
 			}else{
-				System.out.println("File already there");
-				command();
+				System.out.println("Folder already there");
+				
 			}
+			command();
 		}
 		catch(Exception e) {
-			System.out.println("Could not create file");
+			System.out.println("Could not create folder");
 		}
 
 
 	}
 	private void command() {
+		createBat();
+		
 		try {
-			Process p = Runtime.getRuntime().exec("cmd");
-
-		} catch (Exception e) {
+			Runtime.getRuntime().exec("cmd /c start tasks.bat");
+			
+		}catch (Exception e) {
 			// TODO: handle exception
 		}
+		
+//	
 
 	}
+	void createBat() {
+		 try {
+		      File myObj = new File("tasks.bat");
+		      if (myObj.createNewFile()) {
+		        System.out.println("File created: " + myObj.getName());
+		      } else {
+		        System.out.println("File already exists.");
+		      }
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		 try {
+		      FileWriter myWriter = new FileWriter("tasks.bat");
+		      myWriter.write("dir");
+		      System.getProperty( "line.separator" );
+		      myWriter.write("color 02");
+		      System.getProperty( "line.separator" );
+		      myWriter.close();
+		      System.out.println("Successfully wrote to the file.");
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+	}
+	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
